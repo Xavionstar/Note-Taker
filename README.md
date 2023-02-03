378f2d63cff469558003b532b3df3169f21b5f6b
Xavionstar <lincpough@gmail.com>
2023-02-02 23:59:53 -0800

    added the comments and readme page

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
 # Note-Taker
+
+## Description
+This project links a back end express file to a pre established front end set up
+
+## Repo Link
+https://github.com/Xavionstar/Note-Taker
+
+## About the Author
+Lincoln WInter Pough, Express Enthusiast
+
+## Screenshot
+![picture of web page](https://github.com/Xavionstar/public/assets/note-taker.png?raw=true)
--- server.js
+++ server.js
@@ -4,23 +4,23 @@ const app = express()
 const fs = require("fs")
 const { json } = require('express')
 const PORT = process.env.PORT || 3000;
-
+//these functions tell the app to parse in json and set up the public folder
 app.use(express.json())
 app.use(express.static("public"));
-
+//I set up this view engine that i found by googling because it is close to HTML
 app.set('view engine', 'ejs')
-
+//this route gets the home page
 app.get('/', (req, res) => {
-    console.log('here')
+    
     res.render('index')
 })
 
-
+//this route gets the second page
 app.get('/notes', (req, res) => {
     console.log('')
     res.render('notes')
 })
-
+//i set up functions to get the database content and parse it into json
 app.get('/api/notes', (req, res) => {
 
     res.json(getDatabaseContent())
@@ -29,7 +29,7 @@ app.get('/api/notes', (req, res) => {
 app.post('/api/notes', (req, res) => {
     let notes = getDatabaseContent()
 
-
+//I had to create ids because it was not changing when i clicked a note
     let nextId = "0"
     if(notes.length > 0){
         const notesLastIndex = notes.length - 1;
@@ -60,7 +60,7 @@ function getDatabaseContent() {
 function saveDatabaseContent(notes) {
     fs.writeFileSync(__dirname + "/db/db.json", JSON.stringify(notes))
 }
-
+//this function tells it which port to listen to
 app.listen(PORT, ( ) =>{
     console.log(`server running on ${PORT}`)
 })
